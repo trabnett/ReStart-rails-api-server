ea715414285445212c3bd8511cd4ffb83156aa66
trabnett <timrabnett@gmail.com>
2019-04-03 18:18:04 +0000

    tweeking heroku

--- app/controllers/brands/coupons_controller.rb
+++ app/controllers/brands/coupons_controller.rb
@@ -1,10 +1,10 @@
 class Brands::CouponsController < ApplicationController
 
     def new
-        render json: {test: "test"}
-        brand = Brand.find_by(name: params[:brand_name])
-        @brand_coupon = BrandCoupon.create(brand_id: brand.id, code: params[:code], expiary_date: params[:expiry_date,], value: params[:coupon_value])
-        res = {new_coupon: @brand_coupon}
+        render json: {test: "testing"}
+        # brand = Brand.find_by(name: params[:brand_name])
+        # @brand_coupon = BrandCoupon.create(brand_id: brand.id, code: params[:code], expiary_date: params[:expiry_date,], value: params[:coupon_value])
+        # res = {new_coupon: @brand_coupon}
         
     end
 
